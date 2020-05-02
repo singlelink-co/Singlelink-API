91b7a340d6826e7276d66c5e8535218854ad25eb
Jimmy Bisenius <me@jimmybweb.com>
2020-05-02 04:17:12 -0400

    Updated version number

--- singlelink.php
+++ singlelink.php
@@ -3,7 +3,7 @@
    Plugin Name: SingleLink
    Plugin URI: https://singlelink.co
    description: Bridge your content and consumers together with just one link.
-   Version: 0.9.0
+   Version: 0.9.1
    Author: NCLA Group
    Author URI: https://nclagroup.org
    License: unlicensed
