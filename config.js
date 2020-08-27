cb1d510518bd4aed13c4c8d961ff1f3541756d9c
Navid Kabir <navid113@gmail.com>
2020-08-27 15:13:41 -0400

    Update git

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    database: 'mongodb+srv://SinglelinkAdmin:gge1HxDKLT7er3vE@singlelink.hxbyz.gcp.mongodb.net/staging?retryWrites=true&w=majority',
-    secret: 'C# Fucking Sucks'
-};
